def8267d41f18885d54c696f39b28cb2ed952634
chiranz <chiranpoudyal@gmail.com>
2020-02-24 07:11:49 +0530

    Updates

---
+++ precache-manifest.7e0a7c46c6ce9d93ca0e5e16e7951d06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bde66886f4044fa8fd8cde2ac6fddf",
+    "url": "/workout/index.html"
+  },
+  {
+    "revision": "f996b65e0ce38aa7d9e1",
+    "url": "/workout/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "97962dc8e132834f5490",
+    "url": "/workout/static/js/2.3cface54.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/workout/static/js/2.3cface54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f996b65e0ce38aa7d9e1",
+    "url": "/workout/static/js/main.f3f5c3e7.chunk.js"
+  },
+  {
+    "revision": "ec5fe9a59525d6c06547",
+    "url": "/workout/static/js/runtime-main.08305883.js"
+  }
+]);
